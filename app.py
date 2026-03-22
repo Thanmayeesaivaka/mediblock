920ccfb4ea91f9b9435fb2f694fa01c2c1f9d3ac
Thanmayeesaivaka <thanu280305@gmail.com>
2026-03-22 17:28:10 +0530

    med

--- app.py
+++ app.py
@@ -449,5 +449,5 @@ def logout():
 # =====================================
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))   # Render uses PORT
     app.run(host="0.0.0.0", port=port)
